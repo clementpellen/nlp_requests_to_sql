5d780fa3f976aaa02c5ba8b44ff4e11bbca2562c
Clément Pellen <57236778+clementpellen@users.noreply.github.com>
2023-11-22 14:54:18 +0000

    scenario ok

--- database.py
+++ database.py
@@ -1,22 +1,27 @@
 import pandas as pd
 import sqlalchemy
-from sqlalchemy import create_engine, inspect
+from sqlalchemy import create_engine, inspect, text
 
 def create_db():
-    df = pd.read_csv('data.csv')
+    df = pd.read_csv('germany-cars.csv')
     # Create an engine that stores data in the local directory's
-    temp_db = create_engine('sqlite:///:memory:', echo=True)
+    temp_db = create_engine('sqlite:///:memory:', echo=False)
     # Create a table from the dataframe
-    df.to_sql('data', con=temp_db)
+    df.to_sql('cars', con=temp_db)
+    print("\nCREATION DE LA BASE DE DONNEE TERMINEE!\n\n\n\n")
     return temp_db
 
-def get_column_names():
-    temp_db = create_db()
+def get_column_names(temp_db):
     inspector = inspect(temp_db)
-    columns = inspector.get_columns('data')
+    columns = inspector.get_columns('cars')
     column_names = [column['name'] for column in columns]
     column_names = ', '.join(column_names)
     return column_names
+
+def execute_sql_query(temp_db, sql_query):
+    with temp_db.connect() as connection:
+        result = connection.execute(text(sql_query)).fetchall()
+        return result
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-from database import get_column_names
+from database import create_db, get_column_names, execute_sql_query
 
 def generate_sql_query(client, natural_language_query):
     try:
@@ -14,19 +14,30 @@ def generate_sql_query(client, natural_language_query):
                 }
             ],
             model="gpt-3.5-turbo",
+            temperature=0.1,
         )
         return response.choices[0].message.content
     except Exception as e:
         return str(e)
 
+def ask_request():
+    return input("Quelle est la requête que vous souhaitez effectuer ?\n")
+
+def remove_carriage_return(string):
+    return string.replace('\n', ' ')
+
+
 def main():
     load_dotenv()
+    temp_db = create_db()
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-    intro = "Tu es le chatbot de la base de données de l'équipe. Tu dois convertir les requêtes en langage naturel en requêtes SQL. Tu répondras uniquement sous forme de code avce la requête demandée."
-    natural_query = "Quelle est le pays le plus représenté dans la base de données ?"
-    prompt = intro + get_column_names() + natural_query
+    intro = "Tu es le chatbot de la base de données de l'équipe. Tu dois convertir les requêtes en langage naturel en requête SQL. Tu répondras uniquement sous forme de code avce la requête demandée. Utilise la table cars suivante qui ne contient pas d'index"
+    natural_query = ask_request()
+    prompt = intro + get_column_names(temp_db) + natural_query
     sql_query = generate_sql_query(client, prompt)
-    print(sql_query)
+    print("\nRequête SQL générée :\n", sql_query)
+    if input("\nSouhaitez-vous l'exécuter ?\n") == 'oui':
+        print("\nRésultat de la requête :\n", execute_sql_query(temp_db, remove_carriage_return(sql_query)))
 
 if __name__ == "__main__":
     main()
