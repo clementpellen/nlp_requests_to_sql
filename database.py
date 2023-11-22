786a696ffe40af93e51e58cebb1c3527b4c58416
clementpellen <clement.pellen@devoteam.com>
2023-11-22 09:59:27 +0100

    generate requests ok

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import sqlalchemy
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, inspect
 
 def create_db():
     df = pd.read_csv('data.csv')
@@ -10,5 +10,14 @@ def create_db():
     df.to_sql('data', con=temp_db)
     return temp_db
 
+def get_column_names():
+    temp_db = create_db()
+    inspector = inspect(temp_db)
+    columns = inspector.get_columns('data')
+    column_names = [column['name'] for column in columns]
+    column_names = ', '.join(column_names)
+    return column_names
+
+
 if __name__ == "__main__":
-    create_db()
+    print(get_column_names())
--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from database import create_db
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+from database import get_column_names
+
+def generate_sql_query(client, natural_language_query):
+    try:
+        response = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": natural_language_query,
+                }
+            ],
+            model="gpt-3.5-turbo",
+        )
+        return response.choices[0].message.content
+    except Exception as e:
+        return str(e)
 
 def main():
-    print(create_db())
+    load_dotenv()
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+    intro = "Tu es le chatbot de la base de données de l'équipe. Tu dois convertir les requêtes en langage naturel en requêtes SQL. Tu répondras uniquement sous forme de code avce la requête demandée."
+    natural_query = "Quelle est le pays le plus représenté dans la base de données ?"
+    prompt = intro + get_column_names() + natural_query
+    sql_query = generate_sql_query(client, prompt)
+    print(sql_query)
 
 if __name__ == "__main__":
     main()
+
